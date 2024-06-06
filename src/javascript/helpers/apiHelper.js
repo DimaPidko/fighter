6e22f38f1101fe3b2f5af4b448f93882608bc2ef
Dmitriy Pidko <pidkodima@gmail.com>
2024-06-06 11:30:54 +0300

    All: + Done

--- src/javascript/helpers/apiHelper.js
+++ src/javascript/helpers/apiHelper.js
@@ -4,7 +4,7 @@ const BASE_API_URL =
     'https://api.github.com/repos/binary-studio-academy/stage-2-es6-for-everyone/contents/resources/api/';
 const SECURITY_HEADERS = {
     headers: {
-        authorization: 'token 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'
+        authorization: 'token ""'
     }
 };
 
